bf40660be8e79fffd61b95f261bd127ce2194e2c
maveen <maveen@Julias-MacBook.fritz.box>
2023-02-03 19:15:14 +0100

    first commit

--- frontend/views/empty/empty-view.ts
+++ frontend/views/empty/empty-view.ts
@@ -1,15 +1,50 @@
 import { html } from 'lit';
-import { customElement } from 'lit/decorators.js';
+import { customElement, state } from 'lit/decorators.js';
 import { View } from '../../views/view';
+import '@vaadin/vaadin-messages'
+import '@vaadin/vaadin-text-field'
+import "@vaadin/message-list/src/vaadin-message-list";
+import "@vaadin/text-field/src/vaadin-text-field";
+import "@vaadin/message-input/src/vaadin-message-input";
+import Message from 'Frontend/generated/com/example/application/ChatEndpoint/Message';
+import { ChatEndpoint } from 'Frontend/generated/endpoints';
+import { TextFieldChangeEvent } from '@vaadin/text-field/src/vaadin-text-field';
+
 
 @customElement('empty-view')
 export class EmptyView extends View {
+  @state() messages: Message[] = [];
+  @state() userName = '';
+
+  get formattedMessages() {
+    return this.messages.map((m) => ({
+      ...m,
+      time: m.time ? new Date(m.time).toLocaleTimeString('en-US') : 'unknown',
+    }));
+  }
+
+
+
   render() {
-    return html`<div>
-      <img style="width: 200px;" src="images/empty-plant.png" />
-      <h2>This place intentionally left empty</h2>
-      <p>It’s a place where you can grow your own UI 🤗</p>
-    </div>`;
+    return html`
+      <vaadin-message-list class="flex-grow" .items=${this.messages}></vaadin-message-list>
+      <div class="flex p-s gap-s items-baseline">
+        <vaadin-text-field placeholder="Name" @change=${this.userNameChange}></vaadin-text-field>
+        <vaadin-message-input class="flex-grow" @submit=${this.submit}></vaadin-message-input>
+      </div>
+    
+    `;
+  }
+
+  submit(e: CustomEvent) {
+    ChatEndpoint.send({
+      text: e.detail.value,
+      userName: this.userName
+    })
+  }
+
+  userNameChange(e: TextFieldChangeEvent) {
+    this.userName = e.target.value;
   }
 
   connectedCallback() {
@@ -18,11 +53,14 @@ export class EmptyView extends View {
       'flex',
       'flex-col',
       'h-full',
-      'items-center',
-      'justify-center',
-      'p-l',
-      'text-center',
       'box-border'
+    );
+    ChatEndpoint.join().onNext(
+      (message) => {
+        if (message) {
+          this.messages = [...this.messages, message];
+        }
+      }
     );
   }
 }
---
+++ src/main/java/com/example/application/ChatEndpoint.java
@@ -0,0 +1,37 @@
+package com.example.application;
+
+import java.time.Instant;
+
+import com.vaadin.flow.server.auth.AnonymousAllowed;
+
+import dev.hilla.Endpoint;
+import dev.hilla.Nonnull;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Sinks;
+import reactor.core.publisher.Sinks.EmitResult;
+import reactor.core.publisher.Sinks.Many;
+
+@Endpoint
+@AnonymousAllowed
+public class ChatEndpoint {
+
+    public static class Message {
+        public @Nonnull String text;
+        public Instant time;
+        public @Nonnull String userName;
+    }
+
+    private final Many<Message> chatSink = Sinks.many().multicast().directBestEffort();;
+    private final Flux<Message> chat = chatSink.asFlux().replay(10).autoConnect();;
+
+    public @Nonnull Flux<@Nonnull Message> join() {
+        return chat;
+    }
+
+    public void send(Message message) {
+        message.time = Instant.now();
+        chatSink.emitNext(message,
+                (signalType, emitResult) -> emitResult == EmitResult.FAIL_NON_SERIALIZED);
+    }
+
+}
